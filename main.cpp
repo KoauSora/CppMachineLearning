cb68f6e06921b1f858c89db788fab293fe2c58c2
kimin <1390635037@qq.com>
2023-08-11 17:06:49 +0800

    basic modification

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include "cv/Image/Image.h"
+
+using namespace std;
+
+int main() {
+    auto img = Image("24.bmp");
+
+}
---
+++ utils/FileHandle/FileHandle.h
@@ -0,0 +1,14 @@
+//
+// Created by Kimin on 2023/8/10.
+//
+
+#ifndef CML_FILEHANDLE_H
+#define CML_FILEHANDLE_H
+
+
+class FileHandle {
+    char** change_dir(char* path);
+};
+
+
+#endif //CML_FILEHANDLE_H
